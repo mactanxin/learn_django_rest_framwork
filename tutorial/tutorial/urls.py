805a26667aac36d8c8b6ae37b60a557771ad92ec
mactanxin <mactanxin@gmail.com>
2014-09-29 18:09:31 -0700

    finished tutorial part 1

--- tutorial/tutorial/urls.py
+++ tutorial/tutorial/urls.py
@@ -7,6 +7,8 @@ from django.contrib import admin
 urlpatterns = patterns('',
     # Examples:
     # url(r'^$', 'tutorial.views.home', name='home'),
-    # url(r'^^',include('snippets.urls')),
+    # url(r'^blog/', include('blog.urls')),
+
     url(r'^admin/', include(admin.site.urls)),
+    url(r'^', include('snippets.urls')),
 )
